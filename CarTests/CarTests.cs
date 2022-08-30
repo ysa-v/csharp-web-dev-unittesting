b3f8c6254bb2662cc6a87de3dc40734b8a904d5b
ysa-v <ysa.vandenberg@gmail.com>
2022-08-30 11:22:45 -0500

    Added gas tank test

--- CarTests/CarTests.cs
+++ CarTests/CarTests.cs
@@ -1,18 +1,23 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarNS;
 
 namespace CarTests
 {
     [TestClass]
     public class CarTests
     {
-        //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitHub account)
         [TestMethod]
         public void EmptyTest()
         {
             Assert.AreEqual(10, 10, .001);
         }
-        //TODO: constructor sets gasTankLevel properly
 
+        [TestMethod]
+        public void TestInitialGasTank()
+        {
+            Car test_car = new Car("Toyota", "Prius", 10, 50);
+            Assert.AreEqual(10, test_car.GasTankLevel, .001);
+        }
 
         //TODO: gasTankLevel is accurate after driving within tank range
         //TODO: gasTankLevel is accurate after attempting to drive past tank range
